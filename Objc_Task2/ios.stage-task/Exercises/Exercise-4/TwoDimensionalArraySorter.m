34dc0d0848932c5f13d8b0091aa16cf37d615171
Anita Agadzhanyan <aganivo@yandex.ru>
2021-05-17 21:49:53 +0300

    feat: solve 4

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,48 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    //Если пусто, выводим пустой массив
+    if (!array) {
+            return @[];
+        }
+        
+    NSMutableArray *stringArray = [NSMutableArray new];
+    NSMutableArray *numbersArray = [NSMutableArray new];
+    
+    for (NSArray *i in array) {
+        
+        //Если массив одномерный, выводим пустой массив
+        if (![i isKindOfClass:NSArray.class]) {
+            return @[];
+        }
+        
+        for (NSArray *j in i) {
+            //Проверяем, чем является j. Строкой или числом
+            if ([j isKindOfClass:NSString.class]) {
+                [stringArray addObject:j];
+            } else {
+                [numbersArray addObject:j];
+            }
+            
+        }
+    }
+    
+    //Если массив из строк пустой, то сортируем и выводим массив из чисел
+    if (stringArray.count == 0) {
+        [numbersArray sortUsingSelector:@selector(compare:)];
+        return numbersArray;
+    }
+    
+    //Если массив из чисел пустой, то сортируем и выводим массив из строк
+    if (numbersArray.count == 0) {
+        [stringArray sortUsingSelector:@selector(compare:)];
+        return stringArray;
+    }
+    
+    [numbersArray sortUsingSelector:@selector(compare:)];
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"self" ascending:NO];
+    [stringArray sortUsingDescriptors:[NSArray arrayWithObject:sortDescriptor]];
+    return [[NSArray alloc] initWithObjects:numbersArray, stringArray, nil];
 }
 
 @end
